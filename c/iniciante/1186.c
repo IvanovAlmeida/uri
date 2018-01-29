71cda162caa87c3471b182f969c9cf65d1ed4732
Ivanov de Almeida <ivanovdealmeida@gmail.com>
2018-01-28 23:43:49 -0300

    Create 1186.c

---
+++ c/iniciante/1186.c
@@ -0,0 +1,30 @@
+int main() {
+ 
+    float m[12][12], resultado;
+    int i, j, cont = 0, l = 12;
+    char o;
+   
+    scanf("%c", &o);
+    
+    for(i = 0; i < 12; i++){
+        for(j = 0; j < 12; j++){
+            scanf("%f", &m[i][j]);
+        }
+    }
+
+    for(i = 0; i < 12; i++){
+        for(j = l; j < 12; j++){
+            resultado = resultado + m[j][i];
+            cont++;
+        }
+        l--;
+    }
+    
+    if(o == 'M'){
+        resultado = resultado/cont;
+    }
+
+    printf("%.1f\n", resultado);
+
+    return 0;
+}
